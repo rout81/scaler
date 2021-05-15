6bf82621dd648623a3a842570b5ea3da784bc8b3
satanrout <satyaranjanrout81@gmail.com>
2021-05-15 18:18:57 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputData, setInputData] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleInput = (e) => {
+    setInputData(e.target.value);
+    const percentFullhd = Number(e.target.value) / 1920;
+    console.log(inputData, percentFullhd);
+    const resultHd = ((percentFullhd * 1366) / 10).toFixed(6);
+    setResult(resultHd);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="title">scale font size from 1920 to 1366</div>
+        <input value={inputData} onChange={handleInput} type="text" />
+        <div className="text">{result}</div>
+      </div>
     </div>
   );
 }
